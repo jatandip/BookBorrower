22bf9bd9f1d121805b57b587e88af09e2654af9d
Jatan Singh <jatanmultani@gmail.com>
2020-10-31 18:50:43 -0600

    Added login functinality

--- app/src/main/java/com/example/vivlio/LoginActivity.java
+++ app/src/main/java/com/example/vivlio/LoginActivity.java
@@ -2,18 +2,29 @@ package com.example.vivlio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private ImageButton loginBTN;
     private EditText usernameET;
     private EditText passwordET;
     private TextView createAccountTV;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstances) {
@@ -24,9 +35,11 @@ public class LoginActivity extends AppCompatActivity {
         usernameET = findViewById(R.id.LOGIN_ETusername);
         passwordET = findViewById(R.id.LOGIN_ETpassword);
         createAccountTV = findViewById(R.id.LOGIN_TVcreateAccount);
+        mAuth = FirebaseAuth.getInstance();
 
-        String username = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
+
+
+
 
         createAccountTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,13 +52,46 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO handle authentication
-            }
-        });
+                String username = usernameET.getText().toString().trim();
+                String password = passwordET.getText().toString().trim();
+                Log.e("USERNAME", "username is:" + username);
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(LoginActivity.this, "Please enter a username!", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Please enter a password!", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("login Success", "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        openMyBooks();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("Login Failed", "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
 
+
+                            });
+                }
+            }
+
+        });
     }
 
     private void openCreateAccount(){
         Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    private void openMyBooks(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }

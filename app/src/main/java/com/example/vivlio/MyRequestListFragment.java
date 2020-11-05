764c753fbc01bd1f9b08de397513760d8beb33a0
evfloriz <ev.floriz@gmail.com>
2020-11-04 20:22:12 -0700

    Added notifications and changed app theme
    Added notifications with badges. Handled in MainActivity. Monitors
    changes in the database and adds badges to the bottom nav bar on the
    appropriate changes to status. **NOTE** App theme was changed to
    Theme.MaterialComponents derivative to accomodate BadgeDrawable object.
    Colors were set to as close to the same as before.

--- app/src/main/java/com/example/vivlio/MyRequestListFragment.java
+++ app/src/main/java/com/example/vivlio/MyRequestListFragment.java
@@ -45,15 +45,6 @@ import java.util.ArrayList;
  */
 public class MyRequestListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private ArrayList<Book> requestDataList;
     private BookList requestAdapter;
     private FirebaseFirestore db;
@@ -69,16 +60,12 @@ public class MyRequestListFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MyRequestList.
      */
     // TODO: Rename and change types and number of parameters
-    public static MyRequestListFragment newInstance(String param1, String param2) {
+    public static MyRequestListFragment newInstance() {
         MyRequestListFragment fragment = new MyRequestListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -87,14 +74,12 @@ public class MyRequestListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // no params
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_request_list, container, false);
     }

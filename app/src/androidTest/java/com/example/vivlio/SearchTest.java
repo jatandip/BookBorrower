a33fee8b3fa8526ffc3e7dec049852af82666035
Tim van Maren <timvanmaren@hotmail.com>
2020-11-05 16:17:01 -0700

    SearchFragment Test

--- app/src/androidTest/java/com/example/vivlio/SearchTest.java
+++ app/src/androidTest/java/com/example/vivlio/SearchTest.java
@@ -35,18 +35,38 @@ public class SearchTest {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
 
+    /**
+     * Gets the activity
+     * @throws Exception
+     */
     @Test
     public void start() throws Exception{
         Activity activity = rule.getActivity();
     }
 
+    /**
+     * Searches for t, should return result test. Then switches to search books and then finds "g" which shpuld return Game of Thrones.
+     */
     @Test
     public void checkShow() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.navigation_search));
+        assertTrue(solo.waitForText("Users", 1, 2000));
+        solo.enterText((EditText) solo.getView(R.id.searchTermEditText), "t");
+        solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.search_button));
+        assertTrue(solo.waitForText("test", 1, 2000));
+        solo.clickOnView(solo.getView(R.id.search_switch));
+        solo.clearEditText((EditText) solo.getView(R.id.searchTermEditText));
+        solo.enterText((EditText) solo.getView(R.id.searchTermEditText), "g");
+        solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.search_button));
+        assertTrue(solo.waitForText("Game",1,2000));
 
-        assertTrue(solo.waitForText("Users"));
     }
 
+    /**
+     * closes the activities
+     * @throws Exception
+     */
     @After
     public void tearDown() throws Exception{
         solo.finishOpenedActivities();

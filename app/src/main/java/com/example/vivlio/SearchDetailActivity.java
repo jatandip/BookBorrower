535ed8c4a4c5ee4b7febc17c7edc28f6fc1b6bcd
Tim van Maren <timvanmaren@hotmail.com>
2020-11-04 20:33:28 -0700

    Create SearchDetailActivity.java

---
+++ app/src/main/java/com/example/vivlio/SearchDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.vivlio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

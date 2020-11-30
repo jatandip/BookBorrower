5c63c8ec55f42beef34daae50b7630f37457cb30
evfloriz <ev.floriz@gmail.com>
2020-11-30 14:07:00 -0700

    Implemented LocationTest

--- app/src/androidTest/java/com/example/vivlio/LocationTest.java
+++ app/src/androidTest/java/com/example/vivlio/LocationTest.java
@@ -1,6 +1,7 @@
 package com.example.vivlio;
 
 import android.app.Activity;
+import android.widget.EditText;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -8,7 +9,10 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.example.vivlio.Activities.AddBook;
 import com.example.vivlio.Activities.LocationActivity;
+import com.example.vivlio.Activities.LoginActivity;
 import com.example.vivlio.Activities.MainActivity;
+import com.example.vivlio.Activities.Mybook_Accepted;
+import com.example.vivlio.Activities.RequestDetailActivity;
 import com.robotium.solo.Solo;
 import org.junit.After;
 import org.junit.Before;
@@ -33,112 +37,73 @@ public class LocationTest {
     }
 
     @Rule
-    public ActivityTestRule<MainActivity> rule =
-            new ActivityTestRule<>(MainActivity.class, true, true);
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, true, true);
 
+    /**
+     * Log in to vanmaren@ualberta.ca
+     * @throws Exception
+     */
     @Before
     public void SetUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo.assertCurrentActivity("Didn't open to Login Activity", LoginActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.LOGIN_ETusername), "vanmaren@ualberta.ca");
+        solo.enterText((EditText) solo.getView(R.id.LOGIN_ETpassword), "timvm1234");
+        solo.clickOnImageButton(0);
+        solo.assertCurrentActivity("Login failed", MainActivity.class);
     }
 
     /**
-     * Gets the activity
-     *
-     * @throws Exception
+     * Enter the location activity from a RequestDetailActivity and back out
      */
     @Test
-    public void start() throws Exception {
-        Activity activity = rule.getActivity();
+    public void enterFromRequestList() {
+        solo.clickOnView(solo.getView(R.id.navigation_my_request_list));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.sleep(1000);
+        solo.clickOnText("Accepted");
+        solo.sleep(1000);
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Didn't click", RequestDetailActivity.class);
+        solo.sleep(1000);
+
+        solo.clickOnView(solo.getView(R.id.btn_request_location));
+        solo.assertCurrentActivity("Location activity didn't launch", LocationActivity.class);
+        solo.sleep(1000);
+
+        solo.clickOnView(solo.getView(R.id.done_button));
+        solo.assertCurrentActivity("Didn't return from location", RequestDetailActivity.class);
+        solo.sleep(1000);
+
+
     }
 
     /**
-     * tests current activity to make sure it's book
+     * Enter the location activity from a MyBook_Accepted Activity and back out
      */
     @Test
-    public void checkActivity() {
-        solo.assertCurrentActivity("Wrong Activity", AddBook.class);
+    public void enterFromBookList() {
+        solo.clickOnView(solo.getView(R.id.navigation_my_book_list));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.sleep(1000);
+        solo.clickOnText("Accepted");
+        solo.sleep(1000);
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Didn't click", Mybook_Accepted.class);
+        solo.sleep(1000);
+
+        solo.clickOnView(solo.getView(R.id.mapsBtn));
+        solo.assertCurrentActivity("Location activity didn't launch", LocationActivity.class);
+        solo.sleep(1000);
+
+        solo.clickOnView(solo.getView(R.id.done_button));
+        solo.assertCurrentActivity("Didn't return from location", Mybook_Accepted.class);
+        solo.sleep(1000);
     }
 
-    /**
-     * unfinished
-     */
-    @Test
-    public void testMarkerLocation(){
-        LocationActivity location = testLocationDefault();
-        //unsure
-    }
-
-    /**
-     * unfinished
-     */
-    @Test
-    public void testMapReady(){
-        //unsure
-    }
-
-    /**
-     * tests to see if borrower pressing cancel will bring back to initial activity
-     */
-    @Test
-    public void testCancelBorrowerSuccess(){
-        //for borrower
-        LocationActivity location = testLocationDefault();
-        solo.assertCurrentActivity("LocationActivity not open", LocationActivity.class);
-        solo.clickOnButton("Cancel");
-        //location not yet implemented in code
-    }
-
-    /**
-     * tests to see if lender pressing cancel will bring back to initial activity
-     */
-    @Test
-    public void testCancelLenderSuccess(){
-        //for lender
-        LocationActivity location = testLocationSet();
-        solo.assertCurrentActivity("LocationActivity not open", LocationActivity.class);
-        solo.clickOnButton("Cancel");
-        //location not yet implemented in code
-    }
-
-    /**
-     * tests to see if borrower pressing confirm will bring back to initial activity
-     */
-    @Test
-    public void testConfirmBorrowerSuccess(){
-        //for borrower
-        LocationActivity location = testLocationDefault();
-        solo.assertCurrentActivity("LocationActivity not open", LocationActivity.class);
-        solo.clickOnButton("Confirm");
-        //location not yet implemented in code
-    }
-
-    /**
-     * tests to see if lender (after picking location on map) pressing confirm will bring back
-     * to initial activity, returning an intent with longitude and latitude
-     */
-    @Test
-    public void testConfirmLenderSuccess(){
-        //for lender
-        LocationActivity location = testLocationSet();
-        solo.assertCurrentActivity("LocationActivity not open", LocationActivity.class);
-        //make sure marker is on map
-        solo.clickOnButton("Confirm");
-        //location not yet implemented in code, intent to make sure long and lat are correct
-    }
-
-    /**
-     * tests to see if lender (without picking location on map) will not be allowed to return
-     * to initial activity
-     */
-    @Test
-    public void testConfirmLenderFailure(){
-        //for lender
-        LocationActivity location = testLocationSet();
-        solo.assertCurrentActivity("LocationActivity not open", LocationActivity.class);
-        //make sure there is no marker on map
-        solo.clickOnButton("Confirm");
-        solo.assertCurrentActivity("LocationActivity succeeded", LocationActivity.class);
-    }
 
     @After
     public void tearDown() throws Exception{

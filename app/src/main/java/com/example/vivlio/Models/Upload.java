359c54afc4e1c8a1e1533ad94c0ca158be0d3dd0
kishan151999 <kishanp1599@gmail.com>
2020-11-30 14:22:47 -0800

    updated tests

--- app/src/main/java/com/example/vivlio/Models/Upload.java
+++ app/src/main/java/com/example/vivlio/Models/Upload.java
@@ -1,11 +1,23 @@
 package com.example.vivlio.Models;
 
+/**
+ * Upload class used to upload image
+ */
 public class Upload {
     private String name;
     private String imageURL;
 
+
+    /**
+     * constructor for image with no passed in values
+     */
     public Upload(){}
 
+    /**
+     * constructor using name and imageURL
+     * @param name
+     * @param imageURL
+     */
     public Upload(String name, String imageURL){
         if (name.trim().equals("")) {
             this.name = "No Name";
@@ -14,18 +26,34 @@ public class Upload {
         this.imageURL = imageURL;
     }
 
+    /**
+     * get name constructor
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set the name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** getting the image url
+     *
+     * @return
+     */
     public String getImageURL() {
         return imageURL;
     }
 
+    /**
+     * setting the image url
+     * @param imageURL
+     */
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
